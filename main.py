4c9a1fe52953d4d968db17715dad62c62340560a
王昱蘅 <100068319+Enderman-Teleporting@users.noreply.github.com>
2022-07-13 09:28:39 +0800

    Add files via upload
    Known bugs:
    unable to create files(so I deleted the codes)
    more codes to write

---
+++ main.py
@@ -0,0 +1,56 @@
+import os
+
+while True:
+    a=input("")
+    a=a.strip()
+    if "function" in a :
+        print ("暂未开发")
+        continue
+    m=""
+    n=""
+    o=""
+    for word in a:
+        m+=word
+        if len(m)==len(a):
+            if m=="file" or m=="help":
+                print("出错：代码不完整")
+                continue
+            elif m=="list":
+                with open ("list.txt","r") as f:
+                    data=f.read()
+                    print (data)
+                    continue
+            else:
+                print("错误，无此功能")
+                continue
+        elif word==" ":
+            if m=="file ":
+                a=a.lstrip(m)
+                for i in a:
+                    n+=i
+                    if len(n)==len(a):
+                        print("出错：代码不完整")
+                        continue
+                    elif i==" ":
+                        if n=="add ":
+                            m=""
+                            a=a.lstrip(n)
+                            for mu in a:
+                                m+=mu
+                                if len(m)==len(a):
+                                    print("出错：代码不完整")
+                                    continue
+                                elif mu==" ":
+                                    if m=="sort ":
+                                        a=a.lstrip(m)
+                                        for lo in a:
+                                            o+=lo
+                                            if len(a)==len(o):
+                                                try:
+                                                    os.mkdir("./sorts/"+a+"/")
+                                                    print("创建了新的文件夹")
+                                                    continue
+                                                except:
+                                                    print("文件夹生成失败,请查看是否路径有误或同名文件夹已存在")
+                                                    continue
+                                                       
