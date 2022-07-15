9bb5a0c8e0587b968740eec7a550eef9ab7606cd
王昱蘅 <sherrikevin5143@gmail.com>
2022-07-15 22:15:54 +0800

    Total Change
    Another Way

--- main.py
+++ main.py
@@ -1,56 +1,27 @@
-import os
-
+import os,json
+import shutil as shut
+def create_sort(path):
+    try:
+        os.makedirs(path)
+        print("成功创建文件夹")
+    except:
+        print("生成文件夹失败，请检查是否路径有误或同名文件夹已存在")
+def create_file(path):
+    try:
+        with open(os.path.join(path))as file:
+            file.close()
+        print("成功创建文件")
+    except:
+        print("创建文件失败")
+def autorun(filepath,path):
+    f=open("./autorun/autorun.inf","a+")
+    if not "[autorun]" in f:
+        f.write("[autorun]")
+    f.write("open="+path+"\n\n")
+    shut.move(filepath,"./autorun/")
+def directionary(source,directionary):
+    json_string='{"source":'+source+', "directionary":'+directionary+'}'
+    with open ("./directionary.json","a+") as outfile:
+        json.dump(json_string,outfile)
 while True:
     a=input("")
-    a=a.strip()
-    if "function" in a :
-        print ("暂未开发")
-        continue
-    m=""
-    n=""
-    o=""
-    for word in a:
-        m+=word
-        if len(m)==len(a):
-            if m=="file" or m=="help":
-                print("出错：代码不完整")
-                break
-            elif m=="list":
-                with open ("list.txt","r") as f:
-                    data=f.read()
-                    print (data)
-                    break
-            else:
-                print("错误，无此功能")
-                break
-        elif word==" ":
-            if m=="file ":
-                a=a.lstrip(m)
-                for i in a:
-                    n+=i
-                    if len(n)==len(a):
-                        print("出错：代码不完整")
-                        break
-                    elif i==" ":
-                        if n=="add ":
-                            m=""
-                            a=a.lstrip(n)
-                            for mu in a:
-                                m+=mu
-                                if len(m)==len(a):
-                                    print("出错：代码不完整")
-                                    break
-                                elif mu==" ":
-                                    if m=="sort ":
-                                        a=a.lstrip(m)
-                                        for lo in a:
-                                            o+=lo
-                                            if len(a)==len(o):
-                                                try:
-                                                    os.mkdir("./sorts/"+a+"/")
-                                                    print("创建了新的文件夹")
-                                                    break
-                                                except:
-                                                    print("文件夹生成失败,请查看是否路径有误或同名文件夹已存在")
-                                                    break
-                                                       
